b7218ca591de7cd19762c088f298f65c3859ae6a
guanchenz <guanchen@dhcp-128-189-218-246.ubcsecure.wireless.ubc.ca>
2018-03-01 16:13:42 -0800

    setup test templates

---
+++ tests/testthat/test_fit.R
@@ -0,0 +1,12 @@
+# test functions for ssgkmeansr
+
+context("Testing fit function")
+
+# standard_deviation
+test_that("state what the test is doing here", {
+  # expected outputs:
+  # expect_equal(round(standard_deviation(c(1,2,3)),5), 0.8165)
+  
+  # expected errors:
+  # expect_error(standard_deviation(c()), "Zero")
+})
