b035f0f3dddbb088553b984bd67826b9389ae42d
susanfun <susan.fung.data@gmail.com>
2018-03-02 10:59:57 -0800

    Add test scripts for fit function

--- R/ssgkmeansr.R
+++ R/ssgkmeansr.R
@@ -1,7 +1,18 @@
-# ssgkmeansr
+#' Compute k-means clustering
+#' 
+#' Build clusters and save cluster attributes with random selection or k-means ++  centroid initialization
+#'
+#' @param data A data frame with attributes as columns and data points as rows
+#' @param k Number of clusters
+#' @param method Centroid initialization method. `random` or `kmpp``
+#'
+#' @return List containing: 1. data frame of the attributes and clustering for each data point; 2.total within cluster sum of square; 3. data frame of k centroids
+#' @export
+#'
+#' @examples
+#' fit(my_data_frame,3,"kmpp")
 
-fit <- function() {
-  # fit fucntion for kmeans
+fit <- function(data, k, method) {
 }
 
 predict <- function() {
