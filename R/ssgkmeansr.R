0471680d8b0a381666f476703cc2250ac2c1688c
xiaosophiameng <xiaosophiameng@gmail.com>
2018-03-04 02:53:44 -0800

    adjust based on comments

--- R/ssgkmeansr.R
+++ R/ssgkmeansr.R
@@ -7,13 +7,13 @@ fit <- function() {
 #' Predict k-means clustering; provide labels for all observations
 #'
 #' @param data A to-be-predicted new dataframe with attributes as columns and data points as rows. Data without cluster labels
-#' @param the kmeans model with method `random` or `kmpp``
-#'
+#' @param  kmeans model from fit function; a data frame of centroids
+
 #' @return List containing:1. data frame of the attributes；2.clustering label for each data point
 #' @export
 #'
 #' @examples
-#' predict(test_data_frame,3,"random")
+#' kmpredict(test_data_frame, fit_model)
 
 
 predict <- function() {
