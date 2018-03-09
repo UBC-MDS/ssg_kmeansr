79e131e556b9cbf13871e996a931067ac1ffe5c4
xiaosophiameng <xiaosophiameng@gmail.com>
2018-03-09 14:21:42 -0800

    add test script for predict

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -11,6 +11,12 @@ data_bad_string[1,1]<-"susan"
 data_bad_string[2,1]<-"GC"
 data_bad_string[3,2] <-"sophia"
 
+# Bad input - data frame with wrong dimentions
+data_bad_2d<-data_good[, c(2,3)]
+
+# Bad input - empty data frame
+data_bad_empty <- data.frame()
+
 
 context("Testing predict function")
 
@@ -18,15 +24,21 @@ test_that("Predicting results match expectations", {
 	
   # load test data
   
-  test_data <- read_csv('../../data/sample_test.csv')
-  n <-nrow(test_data)
+  n <-nrow(data_good)
   centroids <- as.tibble(rbind(c(0,0),c(1,1),c(-1,-1)))
                          
-  output <- predict(test_data, centroids)
+  output <- predict(data_good, centroids)
   
   # expected outputs:
   expect_equal(nrow(output[,ncol(test_data)]), n) 
   expect_equal(max(output[,ncol(test_data)])<4, TRUE)
   expect_equal(is.data.frame(output), TRUE)
   
+})
+
+test_that('Error in input data', {
+  # expected error:
+  expect_error(kmplot(data_bad_string), 'Input data must be numeric')
+  expect_error(kmplot(data_bad_2d), 'Input data must have 3 columns')
+  expect_error(kmplot(data_bad_empty), 'Input data cannot be empty')
 })
