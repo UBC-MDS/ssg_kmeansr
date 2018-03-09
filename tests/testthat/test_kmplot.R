f2534f2ce94c3f19950d89b380fdfb26480d59f2
susanfun <susan.fung.data@gmail.com>
2018-03-08 23:12:40 -0800

    Combine r code into one. Add expected errors to test_kmplot.R. Fixed a few errors

--- tests/testthat/test_kmplot.R
+++ tests/testthat/test_kmplot.R
@@ -1,11 +1,29 @@
 # test functions for ssgkmeansr
 
+library(tidyverse)
+# Generate test data frame
+# Read in correct data frame
+data_good <- read.csv('../../data/sample_train.csv')
+
+# Bad input - data frame with stings
+data_bad_string<-data_good
+data_bad_string[1,1]<-"susan"
+data_bad_string[2,1]<-"GC"
+data_bad_string[3,2] <-"sophia"
+
+# Bad input - data frame with wrong dimentions
+data_bad_2d<-data_good[, c(2,3)]
+
+# Bad input - empty data frame
+data_bad_empty <- data.frame()
+
+
 context("Testing kmplot function")
 
 test_that("Plot mapping match expectations", {
   # load test data
-  data <- read_csv('../../data/sample_train.csv')
-  p <- kmplot(data)
+  p <- kmplot(data_good)
+  
   # expected outputs:
   expect_equal(toString(p$mapping$x), 'x1')
   expect_equal(toString(p$mapping$y), 'x2')
@@ -13,13 +31,15 @@ test_that("Plot mapping match expectations", {
 })
 
 test_that('Check data integrity', {
-  data <- read_csv('../../data/sample_train.csv')
-  p <- kmplot(data)
-  
+  p <- kmplot(data_good)
+  # expected outputs:
   # check if any data is dropped in the plot
   expect_equal(length(p$data$cluster), nrow(data))
 })
 
 test_that('Error in input data', {
-  expect_error(kmplot(), 'Error in input data')
+  # expected error:
+  expect_error(kmplot(data_bad_string), 'Input data must be numeric')
+  expect_error(kmplot(data_bad_2d), 'Input data must have 3 columns')
+  expect_error(kmplot(data_bad_empty), 'Input data cannot be empty')
 })
